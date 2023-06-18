0c57a1aeddf228b5f9182ee057febdd21fff91d2
MAALATHY <maalathycbe@gmail.com>
2023-06-18 11:57:03 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,6 +3,16 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+TEST_CASE("classifies the Temperature Breach according to CoolingType and Temperature in Celsius"){
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 35) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 40) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -5) == TOO_LOW);
+  
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 45) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 50) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -5) == TOO_LOW);
+
+  REQUIRE(classifyTemperatureBreach(DEFAULT_COOLING, 40) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(DEFAULT_COOLING, 45) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(DEFAULT_COOLING, -5) == TOO_LOW);  
 }
